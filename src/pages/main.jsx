362a43fadd5ae70ed47359d69aabff94dc3a9bcc
amonradev <amonracardoso@gmail.com>
2023-03-08 12:58:25 -0300

    chore: Add path alias in repo

--- src/pages/main.jsx
+++ src/pages/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Home from "./Home/Home";
+import '@config/theme.css'
+import Home from '@pages/Home/Home'
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
