906b3cb80eaafa24014b3a257d32d7f58b386eae
amonradev <amonracardoso@gmail.com>
2023-03-08 13:13:20 -0300

    chore: Configured eslint rules

--- src/pages/main.jsx
+++ src/pages/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import '@config/theme.css'
-import Home from '@pages/Home/Home'
+import "@config/theme.css";
+import Home from "@pages/Home/Home";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
